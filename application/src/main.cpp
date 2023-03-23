d5afde7787e20312d5c76fa999f54f446d427c3c
bsoh <50127167+smtobs@users.noreply.github.com>
2023-03-24 04:02:56 +0900

    Add option parser

--- application/src/main.cpp
+++ application/src/main.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <cstdio>
+#include <unistd.h>
+
 #include "yaml-cpp/yaml.h"
 #include "utils.hpp"
 #include "event_driven.hpp"
@@ -8,36 +11,93 @@ class Core
 public:
     Core(int argc, char **argv) : argc_(argc), argv_(argv)
     {
-        std::string config_path = "../config/core_config.yaml";
-	YAML::Node& config = const_cast<YAML::Node&>(this->config);
-        config = YAML::LoadFile(config_path);
+        if (this->OptionHandler() == false)
+        {
+           exit(-1);
+        }
+
+        YAML::Node& config = const_cast<YAML::Node&>(this->config);
+        config = YAML::LoadFile(this->GetConfigPath());
+
         this->ev_driven = new EventDriven(config);
     }
+
     ~Core()
     {
-	delete this->ev_driven;
+        delete this->ev_driven;
     }
 
     void Start()
     {
-    	ev_driven->StartEventLoop();
+        ev_driven->StartEventLoop();
     }
 
+    void SetConfigPath(std::string& config_path)
+    {
+        this->config_path = config_path;
+    }
+
+    std::string GetConfigPath()
+    {
+        return this->config_path;
+    }
+    
 private:
 	EventDriven* ev_driven;
 	mutable YAML::Node config;
-	//const YAML::Node& config;
-	
+    std::string config_path;
 	int argc_;
 	char** argv_;
+
+    void Help(const char* cmd)
+    {
+        if (cmd == NULL)
+        {
+            std::fprintf(stderr, "Please enter an option.");
+        }
+        std::fprintf(stderr, "Usage : %s [-d -f Config File Name]\n", cmd);
+        std::fprintf(stderr, "  -d,         Background\n");
+        std::fprintf(stderr, "  -f,         Config file path\n");
+        std::fprintf(stderr, "  -h,         Help\n\n");
+    }
+
+    bool OptionHandler()
+    {
+        if (this->argc_ < 1)
+        {
+            return false;
+        }
+
+        int opt;
+        while ((opt = getopt(this->argc_, this->argv_, "dfh:")) != -1)
+        {
+            switch (opt)
+            {
+                case 'd':
+                    //daemonize(); Todo
+                    break;
+
+                case 'f':
+                    this->SetConfigPath(std::string str(optarg));
+                    break;
+
+                case 'h'
+                    this->Help(optarg);
+                    return false;
+
+                default:
+                    std::fprintf(stderr, "Unknown option: %c\n", optopt);
+                    return false;
+            }
+        }
+        return true; 
+    }
 };
 
 int main(int argc, char** argv)
 {	
-Core* core_ = new Core(argc, argv);
-
-	core_->Start();
-	delete core_;
+    std::unique_ptr<Core> core(new Core(argc, argv));
+	core->Start();
 	
     return 0;
 }
