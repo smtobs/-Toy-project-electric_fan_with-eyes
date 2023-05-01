7506b364d696eb3b66381cac54ce42348ab8aa44
bsoh <v4dd14@naver.com>
2023-05-01 23:50:14 +0900

    Custom Board

---
+++ lobby_phone/board/post-image.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -e
+
+START_DIR="$(pwd)"
+BOARD_DIR="$(dirname $0)"
+BOARD_NAME="$(basename ${BOARD_DIR})"
+GENIMAGE_CFG="${BOARD_DIR}/genimage-${BOARD_NAME}.cfg"
+GENIMAGE_TMP="${BUILD_DIR}/genimage.tmp"
+
+# Pass an empty rootpath. genimage makes a full copy of the given rootpath to
+# ${GENIMAGE_TMP}/root so passing TARGET_DIR would be a waste of time and disk
+# space. We don't rely on genimage to build the rootfs image, just to insert a
+# pre-built one in the disk image.
+
+# Gen U-Boot script
+mkimage -A arm -O linux -T script -C none -n "${START_DIR}/board/raspberrypi4/boot.scr" -d "${START_DIR}/board/raspberrypi4/boot.scr" "${BINARIES_DIR}/boot.scr.uimg"
+
+
+trap 'rm -rf "${ROOTPATH_TMP}"' EXIT
+ROOTPATH_TMP="$(mktemp -d)"
+
+rm -rf "${GENIMAGE_TMP}"
+
+genimage \
+	--rootpath "${ROOTPATH_TMP}"   \
+	--tmppath "${GENIMAGE_TMP}"    \
+	--inputpath "${BINARIES_DIR}"  \
+	--outputpath "${BINARIES_DIR}" \
+	--config "${GENIMAGE_CFG}"
+
+exit $?
